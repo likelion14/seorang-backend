3444c13508bd26cb5a300bde517f012cbbcab5da
5y0ung4 <qwerrr504@naver.com>
2026-04-28 14:37:29 +0900

    init: create spring boot project

---
+++ src/main/java/com/likelion/seorang/SeorangApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.seorang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeorangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeorangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/seorang/SeorangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.seorang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeorangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
