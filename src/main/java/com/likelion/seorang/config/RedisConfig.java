64ea9b37e3c79cb70c34723425b4a5cef86b8e98
mxyffff <rua0254@naver.com>
2026-04-29 03:35:13 +0900

    feat: RedisConfig, RefreshTokenRepository 초기 구성

---
+++ src/main/java/com/likelion/seorang/config/RedisConfig.java
@@ -0,0 +1,63 @@
+package com.likelion.seorang.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    // 직접 조작용 (리프레시 토큰)
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+
+    // 캐시용
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        Map<String, RedisCacheConfiguration> cacheConfigs = new HashMap<>();
+
+        // 부스 데이터 - 3시간
+        cacheConfigs.put("booth", defaultConfig().entryTtl(Duration.ofHours(3)));
+
+        // 피드 좋아요 - 2분
+        cacheConfigs.put("likes", defaultConfig().entryTtl(Duration.ofMinutes(2)));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfig().entryTtl(Duration.ofMinutes(30))) // 기본 TTL
+                .withInitialCacheConfigurations(cacheConfigs)
+                .build();
+    }
+
+    private RedisCacheConfiguration defaultConfig() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair
+                                .fromSerializer(
+                                        GenericJacksonJsonRedisSerializer.builder().build()
+                                )
+                );
+    }
+}
---
+++ src/main/java/com/likelion/seorang/repository/RefreshTokenRepository.java
@@ -0,0 +1,42 @@
+package com.likelion.seorang.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+
+@Repository
+@RequiredArgsConstructor
+// Redis 기반으로 리프레시 토큰의 저장/조회/삭제 기능을 제공하는 클래스
+public class RefreshTokenRepository {
+    private final StringRedisTemplate redisTemplate; // Redis 클라이언트
+
+    @Value("${app.jwt.refresh-exp-day}")
+    private long refreshExpDay;
+
+    // 리프레시 토큰 저장
+    public void save(String email, String refreshToken) {
+        redisTemplate.opsForValue().set(
+                key(email), refreshToken,
+                Duration.ofDays(refreshExpDay)
+        );
+    }
+
+    // 리프레시 토큰 조회
+    public String find(String email) {
+        return redisTemplate.opsForValue().get(key(email));
+    }
+
+    // 리프레시 토큰 삭제 (로그아웃/재발급 시 사용)
+    public void delete(String email) {
+        redisTemplate.delete(key(email));
+    }
+
+
+    // 내부 키 생성 규칙
+    private String key(String email) {
+        return "refresh: " + email;
+    }
+}
