2c387d774402cfefa3a13e5b127b2b619c7f8c3c
5y0ung4 <qwerrr504@naver.com>
2026-05-15 11:01:38 +0900

    fix: 필요없는 import 제거

--- src/main/java/com/likelion/seorang/config/RedisConfig.java
+++ src/main/java/com/likelion/seorang/config/RedisConfig.java
@@ -11,7 +11,6 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import tools.jackson.databind.ObjectMapper;
 
 
 import java.time.Duration;
